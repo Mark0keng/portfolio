cc06097676e6cba1c8e16523336db9e7c0c51d76
Farras Arkan <farras.arkan@student.uns.ac.id>
2022-10-14 02:35:37 +0700

    add send contact form feature

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,36 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden')
 })
+
+// Form Submit
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx-9zXTxzG14J6UypEH4bxckxO1YBNMZyks02nesaexbY0aqm2u9dP5BykwQpWqm8kpFA/exec'
+const form = document.forms['contact-form']
+
+const alert = document.querySelector('#my-alert')
+const btnSend = document.querySelector('#btn-send')
+const btnLoading = document.querySelector('#btn-loading')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    
+    // show/hide button
+    btnSend.classList.toggle('hidden')
+    btnLoading.classList.toggle('hidden')
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            // show/hide
+            alert.classList.toggle('hidden')
+            btnSend.classList.toggle('hidden')
+            btnLoading.classList.toggle('hidden')
+            // reset form
+            form.reset()
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+// close alert
+alert.addEventListener('click', function(){
+    alert.classList.toggle('hidden')
+})
