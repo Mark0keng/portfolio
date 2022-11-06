ed96d1477efbfcfc2012727af8d13a778b4ce7ee
Farras Arkan <farras.arkan@student.uns.ac.id>
2022-11-06 20:27:00 +0700

    update dark mode

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header')
     const fixedNav = header.offsetTop
+    const toTop = document.querySelector('#to-top')
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
     } else {
         header.classList.remove('navbar-fixed')
+        toTop.classList.remove('flex')
+        toTop.classList.add('hidden')
     }
 }
 
@@ -51,3 +56,24 @@ form.addEventListener('submit', e => {
 alert.addEventListener('click', function(){
     alert.classList.toggle('hidden')
 })
+
+// Darkmode Toggle
+const darkToggle = document.querySelector('#dark-toggle')
+const html = document.querySelector('html')
+
+darkToggle.addEventListener('click', function() {
+    if(darkToggle.checked) {
+        html.classList.add('dark')
+        localStorage.theme = 'dark'
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light'
+    }
+})
+
+// Toggle position same as mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true
+} else {
+    darkToggle.checked= false   
+}
